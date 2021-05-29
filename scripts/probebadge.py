6c25116aefe3a66443f38cb9654c7870ac2b795d
Baguette <30416854+H-Baguette@users.noreply.github.com>
2021-05-29 17:36:02 +0100

    Create dynamic probe badge script.
    Creates the dynamic probe badge script. It's pretty messy right now, I'll clean it up later.

--- scripts/probebadge.py
+++ scripts/probebadge.py
@@ -1,12 +1,92 @@
 from PIL import Image, ImageFont, ImageDraw
+import requests
+import html2text
+from bs4 import BeautifulSoup
+import re
 
-font = ImageFont.truetype("F25_Bank_Printer.ttf", 16)
 
-img = Image.open('background.png')
+# set headers
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+}
 
-title_text = "AUUUUUGH"
+h = html2text.HTML2Text()
+h.ignore_links = True
+
+# init counters
+hours  = 0
+days   = 0
+weeks  = 0
+months = 0
+years  = 0
+
+output = ''
+
+# query leper's colony
+
+horribleJerk = 227624
+URL = f"https://forums.somethingawful.com/banlist.php?userid={horribleJerk}"
+page = requests.get(URL, headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+
+username = str(soup.find('a', href=f'/member.php?s=&action=getinfo&userid={horribleJerk}').get_text())
+
+# find probations
+probes = re.findall(r'User loses posting privileges for (.*?)\.', soup.prettify())
+#print(probes)
+
+# YES YES I KNOW IM GOING TO TIDY THIS WHEN I KNOW IT WORKS
+for probe in probes:
+    timesplit = probe.split(' ')
+    timesplit[0] = re.sub('[^0-9]','',timesplit[0])
+    if timesplit[1] == 'hours':
+        hours += int(timesplit[0])
+    elif timesplit[1][0:3] == 'day':
+        days += int(timesplit[0])
+    elif timesplit[1][0:4] == 'week':
+        weeks += int(timesplit[0])
+    elif timesplit[1][0:5] == 'month':
+        months += int(timesplit[0])
+    elif timesplit[1][0:4] == 'year':
+        years += int(timesplit[0])
+
+# boring, frustrating math shit
+calcHours = int(hours % 24)
+calcDays = int((hours - calcHours) / 24)
+calcWeeks = int((calcDays - (calcDays % 7)) / 7)
+calcDays = int(calcDays % 7)
+calcMonths = int((calcWeeks - (calcWeeks % 4)) / 4)
+calcWeeks = int(calcWeeks % 4)
+calcYears = int((calcMonths - (calcMonths % 12)) / 12)
+calcMonths = int(calcMonths % 12)
+
+print(f'\nHours: {calcHours}\nDays: {calcDays}\nWeeks: {calcWeeks}\nMonths: {calcMonths}\nYears: {calcYears}')
+#print(h.handle(page))
+
+# figure out what to stick on the badge
+probeTimes = [(str(calcYears) + ' Years'),(str(calcMonths) + ' Months'),(str(calcWeeks) + ' Weeks'),(str(calcDays) + ' Days'),(str(calcHours) + ' Hours')]
+for sentence in probeTimes:
+    if int(re.sub('[^0-9]','',sentence)) > 0:
+        if output != '':
+            output += f', {str(sentence)}'
+        else:
+            output += str(sentence)
+
+uNameFont = ImageFont.truetype("F25_Bank_Printer.ttf", 16)
+timeFont = ImageFont.truetype("F25_Bank_Printer.ttf", 12)
+
+img = Image.open('badgebg_simple.png')
+
 image_editable = ImageDraw.Draw(img)
-image_editable.text((15,15), title_text, (237,230,211), font=font)
+image_editable.text((100,15), username, (0,0,0), font=uNameFont)
+image_editable.text((100,35), 'Total time probated:', (0,0,0), font=timeFont)
+image_editable.text((100,50), output, (0,0,0), font=timeFont)
+if calcYears > 1:
+    image_editable.text((100,62), 'Jesus Christ.', (0,0,0), font=timeFont)
 
-img.show()
-img.save('fuckyou.png')
+img.save('probebadge.png')
