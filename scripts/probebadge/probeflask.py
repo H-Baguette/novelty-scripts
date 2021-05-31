d346af448a9cd211d0fa4cb3307a7f264230082c
hbag <30416854+h-bag@users.noreply.github.com>
2021-05-31 04:03:29 +0100

    Update probeflask.py

--- scripts/probebadge/probeflask.py
+++ scripts/probebadge/probeflask.py
@@ -10,12 +10,13 @@ except ImportError:
     from site_packages import requests_cache
 import html2text
 from bs4 import BeautifulSoup
+import datetime
 import re
 import os
 import sys
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = False
+app.config["DEBUG"] = True
 
 ids = {'userid': 0}
 scriptPath = os.path.dirname(__file__)
@@ -40,6 +41,7 @@ def generateBadge(userid):
     months = 0
     years  = 0
 
+    username = 'A Goody Two-Shoes'
     output = ''
     print(scriptPath)
     print(f'{scriptPath}test')
@@ -54,53 +56,47 @@ def generateBadge(userid):
         print("UserIDs can only be integers.")
         return
     print(f'{scriptPath}/badges/{horribleJerk}.png')
-    URL = f"https://forums.somethingawful.com/banlist.php?userid={horribleJerk}"
-    page = requests.get(URL, headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #print(soup.prettify())
+    pageNum = 1
+    probes=[]
+    endOfSheet = False
+    while endOfSheet != True:
+        URL = f"https://forums.somethingawful.com/banlist.php?&sort=&asc=0&adminid=&ban_month=0&ban_year=0&actfilt=-1&userid={horribleJerk}&pagenumber={str(pageNum)}"
+        page = requests.get(URL, headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        #print(soup.prettify())
+        
+        if re.findall(r'User loses posting privileges for (.*?)\.', soup.prettify()) == []:
+            endOfSheet = True
+            break
 
-    username = str(soup.find('a', href=f'/member.php?s=&action=getinfo&userid={horribleJerk}').get_text())
+        username = str(soup.find('a', href=f'/member.php?s=&action=getinfo&userid={horribleJerk}').get_text())
 
-    # find probations
-    probes = re.findall(r'User loses posting privileges for (.*?)\.', soup.prettify())
+        # find probations
+        probeScrape = re.findall(r'User loses posting privileges for (.*?)\.', soup.prettify())
+        probes += probeScrape
+        pageNum += 1
     #print(probes)
 
     # YES YES I KNOW IM GOING TO TIDY THIS WHEN I KNOW IT WORKS
+    elapsed = timedelta(years=0,months=0,weeks=0,days=0,hours=0)
     for probe in probes:
         timesplit = probe.split(' ')
         timesplit[0] = re.sub('[^0-9]','',timesplit[0])
-        if timesplit[1] == 'hours':
-            hours += int(timesplit[0])
-        elif timesplit[1][0:3] == 'day':
-            days += int(timesplit[0])
-        elif timesplit[1][0:4] == 'week':
-            weeks += int(timesplit[0])
-        elif timesplit[1][0:5] == 'month':
-            months += int(timesplit[0])
-        elif timesplit[1][0:4] == 'year':
-            years += int(timesplit[0])
-
-    # boring, frustrating math shit
-    calcHours = int(hours % 24)
-    calcDays = int((hours - calcHours) / 24)
-    calcWeeks = int((calcDays - (calcDays % 7)) / 7)
-    calcDays = int(calcDays % 7)
-    calcMonths = int((calcWeeks - (calcWeeks % 4)) / 4)
-    calcWeeks = int(calcWeeks % 4)
-    calcYears = int((calcMonths - (calcMonths % 12)) / 12)
-    calcMonths = int(calcMonths % 12)
-
-    print(f'\nHours: {calcHours}\nDays: {calcDays}\nWeeks: {calcWeeks}\nMonths: {calcMonths}\nYears: {calcYears}')
-    #print(h.handle(page))
-
-    # figure out what to stick on the badge
-    probeTimes = [(str(calcYears) + ' Years'),(str(calcMonths) + ' Months'),(str(calcWeeks) + ' Weeks'),(str(calcDays) + ' Days'),(str(calcHours) + ' Hours')]
-    for sentence in probeTimes:
-        if int(re.sub('[^0-9]','',sentence)) > 0:
-            if output != '':
-                output += f', {str(sentence)}'
-            else:
-                output += str(sentence)
+        (unit, value) = (timesplit[1], int(timesplit[0]))
+        if   'hours' in unit: elapsed += timedelta(hours = value)
+        elif 'day'   in unit: elapsed += timedelta(days  = value)
+        elif 'week'  in unit: elapsed += timedelta(weeks = value)
+        elif 'month' in unit: elapsed += timedelta(month = value)
+        elif 'year'  in unit: elapsed += timedelta(years = value)
+    output = 
+        [str(x[0]) + ' ' + x[1] for x in [(elapsed.years,  'Years'),
+                                          (elapsed.months, 'Months'),
+                                          (elapsed.weeks,  'Weeks'),
+                                          (elapsed.days,   'Days'),
+                                          (elapsed.hours,  'Hours')] if x[0] != 0].join(', ')
+    
+    if output == '':
+        output = 'This SQUARE hasn\'t been probated before.'
 
     uNameFont = ImageFont.truetype(f"{scriptPath}/F25_Bank_Printer.ttf", 16)
     timeFont = ImageFont.truetype(f"{scriptPath}/F25_Bank_Printer.ttf", 12)
@@ -110,12 +106,12 @@ def generateBadge(userid):
 
     image_editable = ImageDraw.Draw(img)
     image_editable.text((100,15), username, (0,0,0), font=uNameFont)
-    image_editable.text((100,35), 'Total time probated*:', (0,0,0), font=timeFont)
+    image_editable.text((100,35), 'Total time probated:', (0,0,0), font=timeFont)
     image_editable.text((100,50), output, (0,0,0), font=timeFont)
     if calcYears > 1:
         image_editable.text((100,62), 'Jesus Christ.', (0,0,0), font=timeFont)
 
-    image_editable.text((275,80), '*only counts last 50 probes', (150,150,150), font=timeFont)
+    #image_editable.text((275,80), '*only counts last 50 probes', (150,150,150), font=timeFont)
 
     img.save(f'{scriptPath}/badges/{horribleJerk}.png')
     print(f'{scriptPath}/badges/{horribleJerk}.png')
@@ -125,7 +121,7 @@ def generateBadge(userid):
 
 @app.route('/', methods=['GET'])
 def home():
-    return '<html><body style="background-image:url(\'https://anlucas.neocities.org/compspin_e0.gif\');background-size:cover;"><p style="color:white;">User ID here idiot (NOT your username):</p><form action="https://sa-probebadge.herokuapp.com/api/probebadge" method="GET"><input type="text" name="userid"/><input type="submit"/></form></body><html>'
+    return '<html><body style="background-image:url(\'https://anlucas.neocities.org/compspin_e0.gif\');background-size:cover;"><p style="color:white;">User ID here idiot (NOT your username):</p><form action="https://probebadge-dev.herokuapp.com/api/probebadge" method="GET"><input type="text" name="userid"/><input type="submit"/></form><p style="color:white;"><b>MAKE SURE YOU WRAP THE URL YOU\'RE GIVEN IN [IMG] TAGS!</b></p></body><html>'
  
 @app.route('/api/probebadge', methods=['POST', 'GET'])
 def api_genbadge():
@@ -135,6 +131,6 @@ def api_genbadge():
         else:
             return (generateBadge(int(flask.request.args['userid']))), 201, {'Access-Control-Max-Age': '3600'}
     except (ValueError, AttributeError):
-        return '<h1>handle <i>this</i> you fucking charlatan</h1><br/><img src="http://goatse.info/hello.jpg">'
+        return '<center><h1>Invalid UserID</h1><p>You <i>did</i> enter your UserID, and not your username, <i>right?</i></p><br/><img src="https://i.imgur.com/l0wWcPl.png"></center>'
  
 app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000))
