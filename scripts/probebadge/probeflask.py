f820c4c2b83dbc50adb6b4907f7bf93f8755ac7b
hbag <30416854+h-bag@users.noreply.github.com>
2021-05-31 04:43:16 +0100

    Update probeflask.py

--- scripts/probebadge/probeflask.py
+++ scripts/probebadge/probeflask.py
@@ -93,7 +93,7 @@ def generateBadge(userid):
         elif timesplit[1][0:4] == 'year':
             pyears += int(timesplit[0])
     
-    output = humanize.naturaldelta(dt.timedelta(hours=phours,days=pdays,weeks=pweeks,months=pmonths,years=pyears))
+    output = humanize.naturaldelta(dt.timedelta(hours=phours,days=(pdays + (365 * pyears)),weeks=(pweeks + (4 * pmonths))))
     
     if output == '':
         output = 'This SQUARE hasn\'t been probated before.'
